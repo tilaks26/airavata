2defa8f1d85fbbaa015519d438d518953b6bb664
Ajinkya Dhamnaskar <adhamnas@apache.org>
2017-05-10 12:12:57 -0400

    Adding test for task implementation reflection

--- modules/worker/worker-messaging/src/test/java/TestWorkerMessaging.java
+++ modules/worker/worker-messaging/src/test/java/TestWorkerMessaging.java
@@ -1,6 +1,13 @@
 import org.apache.airavata.common.exception.AiravataException;
 import org.apache.airavata.model.task.TaskTypes;
+import org.apache.airavata.worker.core.exceptions.WorkerException;
+import org.apache.airavata.worker.core.task.Task;
+import org.apache.airavata.worker.core.utils.WorkerFactory;
 import org.apache.airavata.worker.messaging.utils.WorkerMessagingFactory;
+import org.apache.airavata.worker.task.datastaging.impl.DataStageTask;
+import org.apache.airavata.worker.task.datastaging.utils.DataStagingFactory;
+import org.apache.airavata.worker.task.envsetup.impl.EnvironmentSetupTask;
+import org.apache.airavata.worker.task.jobsubmission.impl.DefaultJobSubmissionTask;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,17 +19,36 @@ import java.util.List;
  */
 public class TestWorkerMessaging {
 
+//    @Test
+//    public void testSubscriberCreation(){
+//        List<String> tasks = new ArrayList<String>(){{add(TaskTypes.ENV_SETUP.toString());add(TaskTypes.DATA_STAGING.toString());}};
+//        try {
+//
+//            WorkerMessagingFactory.createSubscribers(tasks);
+//            Assert.assertEquals(WorkerMessagingFactory.getSubscriberMap().size(), tasks.size());
+//
+//        } catch (AiravataException e) {
+//            Assert.fail("Fail to start subscribers.", e);
+//        }
+//    }
+
     @Test
-    public void testSubscriberCreation(){
-        List<String> tasks = new ArrayList<String>(){{add(TaskTypes.ENV_SETUP.toString());add(TaskTypes.DATA_STAGING.toString());}};
+    public void testTaskImplementationFactory(){
+
         try {
+            WorkerMessagingFactory.loadConfiguration();
+            Task task = WorkerMessagingFactory.getTaskImplementation(TaskTypes.ENV_SETUP);
+            Assert.assertEquals(task instanceof EnvironmentSetupTask, true);
 
-            WorkerMessagingFactory.createSubscribers(tasks);
-            Assert.assertEquals(WorkerMessagingFactory.getSubscriberMap().size(), tasks.size());
+            task = WorkerMessagingFactory.getTaskImplementation(TaskTypes.DATA_STAGING);
+            Assert.assertEquals(task instanceof DataStageTask, true);
 
-        } catch (AiravataException e) {
-            Assert.fail("Fail to start subscribers.", e);
+            task = WorkerMessagingFactory.getTaskImplementation(TaskTypes.JOB_SUBMISSION);
+            Assert.assertEquals(task instanceof DefaultJobSubmissionTask, true);
+        } catch (WorkerException e) {
+            Assert.fail("Failed to load worker configuration.", e);
         }
+
     }
 
 }
